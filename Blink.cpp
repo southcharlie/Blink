b64b84bb59020a203191c9760922162f31c26b29
southcharlie <37132936+southcharlie@users.noreply.github.com>
2018-03-08 17:38:17 +0900

    Add files via upload

---
+++ Blink.cpp
@@ -0,0 +1,12 @@
+#include "Arduino.h"
+#include "Blink.h"
+void setup(){
+  blinkInit();
+}
+void loop(){
+  LEDon();
+  delay(1000);
+  LEDoff();
+  delay(1000);
+}
+
---
+++ Blink.h
@@ -0,0 +1,10 @@
+void blinkInit(){
+  pinMode(LED_BUILTIN,OUTPUT);
+}
+void LEDon(){
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+void LEDoff(){
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
